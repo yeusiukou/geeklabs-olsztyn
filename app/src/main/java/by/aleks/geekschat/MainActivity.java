46c0e97f406bd45b08cbbdf9df513feee3da3ab6
Aliaksei Yeusiukou <xroker@gmail.com>
2016-05-15 12:45:35 +0200

    Add chat layout

--- app/src/main/java/by/aleks/geekschat/MainActivity.java
+++ app/src/main/java/by/aleks/geekschat/MainActivity.java
@@ -3,7 +3,6 @@ package by.aleks.geekschat;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -22,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        setTitle("My friends");
+        setTitle(getString(R.string.my_friends));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
