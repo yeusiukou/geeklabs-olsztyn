37f9a6c059cb927f999a9397d06b98aa7acdb17c
Aliaksei Yeusiukou <xroker@gmail.com>
2016-05-15 12:22:19 +0200

    Implement main activity logic

--- app/src/main/java/by/aleks/geekschat/MainActivity.java
+++ app/src/main/java/by/aleks/geekschat/MainActivity.java
@@ -1,13 +1,20 @@
 package by.aleks.geekschat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+import io.kimo.lib.faker.Faker;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView friendsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +22,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setTitle("My friends");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent usersIntent = new Intent(MainActivity.this, UsersActivity.class);
+                startActivity(usersIntent);
             }
         });
+        friendsListView = (ListView)findViewById(R.id.mainList);
+        UsersAdapter usersAdapter = new UsersAdapter(this, R.layout.item_user, getFriends());
+        friendsListView.setAdapter(usersAdapter);
+    }
+
+    private ArrayList<User> getFriends(){
+
+        ArrayList<User> users = new ArrayList<>();
+        for(int i=0; i<5; i++){
+            users.add(new User(Faker.with(this).Name.fullName(), Faker.Internet.email(), 5, Faker.Url.avatar()));
+        }
+
+        return users;
     }
 
 }
